80f1c4561ef1eff2f4a1fe05d3a614c616dcebe7
Rintu <Rintu@VINDUJA>
2022-11-19 15:59:48 +0530

    Add project files.

---
+++ Test3_Vinay_Arithmetic_Exceptions/Arithmetic_Exception.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test3_Vinay_Arithmetic_Exceptions
+{
+    public class Arithmetic_Exception
+    {
+        public void DevisionOfTwoNumbers()
+        {
+            Console.WriteLine("---DevisionOfTwoNumbers---");
+            try
+            {
+                int num1; int num2; int total;
+                Console.WriteLine("Enter num1 value:");
+                num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter num2 value:");
+                num2 = Convert.ToInt32(Console.ReadLine());
+                total = num1 / num2; Console.WriteLine("the result is :" + total);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Num2 Should not be Zero'0' ");
+            }
+        }
+        public void SumOfTwoNumbers()
+        {
+            Console.WriteLine("---SumOfTwoNumbers---");
+            try
+            {
+                int num1; int num2;
+                Console.WriteLine("Enter num1 value:");
+                num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter num2 value:");
+                num2 = Convert.ToInt32(Console.ReadLine());
+                
+
+                int total = num1 - num2 ;
+                Console.WriteLine("the result is :" + total);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Enter Numbers to all Variables, like 1,2,3...");
+            }
+        }
+        
+    } 
+}
+
+
+
+
+
---
+++ Test3_Vinay_Arithmetic_Exceptions/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Test3_Vinay_Arithmetic_Exceptions;
+
+Console.WriteLine("Hello, World!");
+
+
+Arithmetic_Exception objectArithmetic = new Arithmetic_Exception();
+objectArithmetic.DevisionOfTwoNumbers();
+objectArithmetic.SumOfTwoNumbers();
